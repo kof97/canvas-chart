b17026893cbe97361940a2f001e39d721d1bde2a
kof97 <1048434786@qq.com>
2016-07-03 11:57:28 +0800

    init

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,10 @@ var chart = (function() {
 			width: 700,
 			height: 400,
 			axisX: [1, 2, 3, 4, 5, 6, 7],
+			axisY: ['20%', '40%', '60%', '80%', '100%'],
 
+			fill: [35.5, 48, 11, 26, 99, 88, 67],
+			fillColor: "#b5cb85"
 		},
 
 		canvas, 
@@ -18,16 +21,22 @@ var chart = (function() {
 		canvas.width = data.width;
 		canvas.height = data.height;
 
-		scaleX = canvas.width / 10;
-		scaleY = canvas.height / 10;
+		scaleX = canvas.width / (data.axisX.length + 3);
+		scaleY = canvas.height / (data.axisY.length + 3);
 
 		ctx = canvas.getContext('2d');
 
 		drawX(data.axisX);
+		drawY(data.axisY);
+		drawBg(data.axisY);
 
-
+		drawRect(data.fill, data.fillColor);
 
 	};
+
+	var drawRect = function(fill, color) {
+
+	}
 
 	var drawX = function(axisX) {
 		ctx.beginPath();
@@ -35,7 +44,16 @@ var chart = (function() {
 		ctx.moveTo(scaleX, canvas.height - scaleY);
 		ctx.lineTo(canvas.width - scaleX, canvas.height - scaleY);
 
+		var len = axisX.length;
+		for (var i = 0; i < len; i++) {
+			ctx.font = "normal normal bold 14px 微软雅黑";
+            ctx.fillStyle = "#285ea6";
 
+			ctx.fillText(axisX[i], scaleX * (i + 2), canvas.height - scaleY + 20, 24);
+
+
+
+		};
 
 		ctx.stroke();
 
@@ -44,6 +62,47 @@ var chart = (function() {
 
 	}
 
+	var drawY = function(axisY) {
+		ctx.beginPath();
+
+		ctx.moveTo(scaleX, scaleY);
+		ctx.lineTo(scaleX, canvas.height - scaleY);
+
+		var len = axisY.length;
+		var fontSize = 15;
+		for (var i = 0; i < len; i++) {
+			ctx.font = "normal normal bold " + fontSize + "px 微软雅黑";
+            ctx.fillStyle = "#285ea6";
+
+            var x = scaleX - fontSize * 2.5;
+            var y = canvas.height - scaleY * (i + 2) + fontSize / 3;
+			ctx.fillText(axisY[i], x, y);
+            
+		};
+
+		ctx.stroke();
+
+
+		ctx.closePath();
+
+	}
+
+	var drawBg = function(axisY) {
+		var len = axisY.length;
+
+		ctx.beginPath();
+		ctx.fillStyle = "#fff";
+
+		for (var i = 0; i < len; i++) {
+			if (i % 2 == 0) {
+				ctx.fillRect(scaleX + 1, scaleY * (i + 2), canvas.width - 2 * scaleX, scaleY);
+                ctx.fill();
+			};
+		};
+
+		ctx.closePath();
+
+	}
 
 	init(data);
 
